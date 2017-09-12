d27eb3646419e0eb6cb620da39fc256044fc019a
Firefly4266 <gaines.johnp@gmail.com>
2017-09-12 12:23:57 -0700

    Michelle problem 12 completed.

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -10,18 +10,34 @@ namespace DecisionStructures
     {
         static void Main(string[] args)
         {
+            decimal discount;
             Console.Write("Hello user, please enter the price of the item purchased: ");
             var itemPrice = Console.ReadLine();
             var price = decimal.Parse(itemPrice);
             Console.Write("How many products did you purchase at this price? ");
             var itemAmount = Console.ReadLine();
             var amount = decimal.Parse(itemAmount);
+            var purchacePrice = amount * price;
             if (amount > 5 && price > 100)
             {
-                Console.WriteLine("You get a discount!");
+                if (purchacePrice > 10000)
+                {
+                    discount = purchacePrice * 0.2m;
+                    Console.WriteLine("Your discount is: ${0} on a ${1} purchase", discount, purchacePrice);
+                }
+                else if (purchacePrice < 10000 && purchacePrice > 1000)
+                {
+                    discount = purchacePrice * 0.1m;
+                    Console.WriteLine("Your discount is: ${0} on a ${1} purchase", discount, purchacePrice);
+                }
+                else if (purchacePrice < 1000 && purchacePrice > 100)
+                {
+                    discount = purchacePrice * 0.05m;
+                    Console.WriteLine("Your discount is: ${0} on a ${1} purchase", discount, purchacePrice);
+                }
+                else
+                    Console.WriteLine("You do not get a discount.");
             }
-            else
-                Console.WriteLine("You do not get a discount.");
         }
     }
 }
