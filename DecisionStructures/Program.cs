5b8976945f911566312d8b56fb5b49b79b4ad643
Firefly4266 <gaines.johnp@gmail.com>
2017-09-12 11:26:11 -0700

    Starter code

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -10,13 +10,7 @@ namespace DecisionStructures
     {
         static void Main(string[] args)
         {
-            Console.Write("Hello user, please input a number and I'll tell you if it's even or odd: ");
-            var userInput = Console.ReadLine();
-            var input = int.Parse(userInput);
-            if (input % 2 == 0)
-                Console.WriteLine("Your number is even");
-            else
-                Console.WriteLine("Your number is odd.");
+            
         }
     }
 }
