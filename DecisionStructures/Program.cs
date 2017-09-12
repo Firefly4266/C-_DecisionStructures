0253a4ca1400a3e10db835603911fe46d41ab815
Firefly4266 <gaines.johnp@gmail.com>
2017-09-12 10:25:06 -0700

    Michelle problem 8 completed

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -13,18 +13,13 @@ namespace DecisionStructures
             Console.Write("Hello user, what is your favorite number?  ");
             var userInput = Console.ReadLine();
             var input = int.Parse(userInput);
+
             if (input > 1000)
-            {
                 Console.WriteLine("Wow! That's a big number.");
-            }
             else if (input < 0)
-            {
                 Console.WriteLine("Don't be so negative");
-            }
             else
-            {
                 Console.WriteLine("That's a nice number.");
-            }
         }
     }
 }
