e272452969828af5cd6a5ae353f548160e6cbf19
Firefly4266 <gaines.johnp@gmail.com>
2017-09-12 10:10:22 -0700

    Michelle problem 7 completed.

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -10,6 +10,21 @@ namespace DecisionStructures
     {
         static void Main(string[] args)
         {
+            Console.Write("Hello user, what is your favorite number?  ");
+            var userInput = Console.ReadLine();
+            var input = int.Parse(userInput);
+            if (input > 1000)
+            {
+                Console.WriteLine("Wow! That's a big number.");
+            }
+            else if (input < 0)
+            {
+                Console.WriteLine("Don't be so negative");
+            }
+            else
+            {
+                Console.WriteLine("That's a nice number.");
+            }
         }
     }
 }
