2af7d0309e74f81cbba57e72eabbc02d7be8f329
Firefly4266 <gaines.johnp@gmail.com>
2017-09-12 11:34:58 -0700

    Michelle problem 10 completed.

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -10,7 +10,10 @@ namespace DecisionStructures
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Hello user, quick question...what is the result of 8 * 5?  ");
+            var userInput = Console.ReadLine();
+            var input = int.Parse(userInput);
+            Console.WriteLine((input != 40) ? "Thanks, I had a brain freeze!" : "I don't think that right, I'll shoot one of my kids a text...they'll know.");
         }
     }
 }
