6cc96213ab5ab9b6f28109d96187d1b65369f749
Firefly4266 <gaines.johnp@gmail.com>
2017-09-12 11:48:31 -0700

    Michelle problem 11 completed.

--- DecisionStructures/Program.cs
+++ DecisionStructures/Program.cs
@@ -10,7 +10,18 @@ namespace DecisionStructures
     {
         static void Main(string[] args)
         {
-
+            Console.Write("Hello user, please enter the price of the item purchased: ");
+            var itemPrice = Console.ReadLine();
+            var price = decimal.Parse(itemPrice);
+            Console.Write("How many products did you purchase at this price? ");
+            var itemAmount = Console.ReadLine();
+            var amount = decimal.Parse(itemAmount);
+            if (amount > 5 && price > 100)
+            {
+                Console.WriteLine("You get a discount!");
+            }
+            else
+                Console.WriteLine("You do not get a discount.");
         }
     }
 }
